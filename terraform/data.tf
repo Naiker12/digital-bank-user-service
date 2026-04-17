1049c516237ed2cbd18684ceceddffe7402da605
naiker12 <gomeznaiker@gmail.com>
2026-04-17 10:38:58 -0500

    feat: implement Terraform infrastructure for user service including DynamoDB, S3, and Lambda functions

--- terraform/data.tf
+++
@@ -1,5 +0,0 @@
-data "archive_file" "user_service_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../deployment_package"
-  output_path = "${path.module}/user_service.zip"
-}
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "user_table_name" {
-  value = aws_dynamodb_table.user_table.name
-}
-
-output "user_avatars_bucket" {
-  value = aws_s3_bucket.user_avatars.id
-}
